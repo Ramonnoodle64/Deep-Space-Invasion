4e9228a917f559e378a34a401e4eb564ac9d8c20
Grant Holmen <76141856+Ramonnoodle64@users.noreply.github.com>
2021-12-30 20:29:05 -0800

    Added pause menu

--- runner.py
+++ runner.py
@@ -10,15 +10,15 @@ WIDTH, HEIGHT = 750, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Fake Space Invaders")
 
-BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT) )
-
+BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
     
-def main():
+def main(): 
     run = True
     FPS = 60
     lost = False
     won = False
     new_level = False
+    pause = False
     
     stop_timer = 0
     level = 0
@@ -27,10 +27,12 @@ def main():
     
     main_font = pygame.font.SysFont("arial", 40)
     end_font = pygame.font.SysFont("arial", 80)
+    title_font_large = pygame.font.SysFont("comicsans", 70)
+    title_font = pygame.font.SysFont("comicsans", 40)
     wave_font = pygame.font.SysFont("comicsans", 100)
     
     laser_velocity = 6
-    laser_velocity2 = 8
+    laser_velocity_player = 8
     player_velocity = 4
     fire_rate = 7
     wave_length = 5
@@ -66,7 +68,7 @@ def main():
         lives_label = main_font.render(f"Lives- {lives}", 1, (240,240,240))
         level_label = main_font.render(f"Level {level}", 1, (240,240,240))
         WIN.blit(lives_label, (20, 10))
-        WIN.blit(level_label, (600, 10))
+        WIN.blit(level_label, (590, 10))
         
         for enemy in enemies:
             enemy.draw(WIN)
@@ -76,6 +78,17 @@ def main():
             
         player.draw(WIN)
         
+        if pause == True:
+            WIN.blit(BACKGROUND, (0,0))
+            pause_label = title_font_large.render("PAUSE", 1, (250,250,250))
+            WIN.blit(pause_label, (WIDTH/2 - pause_label.get_width()/2, 150))
+            WIN.blit(lives_label, (20, 10))
+            WIN.blit(level_label, (590, 10))
+            continue_label = title_font.render("Press enter to continue ", 1, (220,220,220))
+            continue_label2 = title_font.render("Press esc again to go to the Main Menu.. ", 1, (200,200,200))
+            WIN.blit(continue_label, (WIDTH/2 - continue_label.get_width()/2, 360))
+            WIN.blit(continue_label2, (WIDTH/2 - continue_label2.get_width()/2, 400))
+            
         if new_level == True:
             new_level_label = wave_font.render(f"Wave {wave}", 1, (255,255,255))
             WIN.blit(new_level_label, (WIDTH/2 - new_level_label.get_width()/2, 350))
@@ -95,7 +108,7 @@ def main():
         clock.tick(FPS)
         draw_window()
         
-        # Pauses if there is a new level, the player has won, or the player has lost 
+        # Pauses if there is a new level, the player has won, or the player has lost
         if new_level:
             if stop_timer > FPS*2:
                 new_level = False
@@ -121,6 +134,20 @@ def main():
             else: 
                 continue
         
+        if pause:
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_RETURN]:
+                pause = False
+                stop_timer = 0
+            elif keys[pygame.K_ESCAPE] and stop_timer > 30:
+                run = False
+            else:
+                stop_timer += 1
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        quit()
+                continue
+            
         # Spawns enemies and handles difficulty ramp with new waves
         if len(enemies) == 0:
             
@@ -130,11 +157,11 @@ def main():
 
             if level % 10 == 0 and level != 0:
                 fire_rate -= 1
+                player.health = player.max_health
                 
             if level % 5 == 0 and level != 0:
                 wave_range -= 500
                 Enemy.shift += .5
-                player.health = player.max_health
                 if Player.max_cooldown > 10:
                     Player.max_cooldown -= 5
                     
@@ -168,7 +195,7 @@ def main():
         
         # Returns to main menu if esc is pressed
         if keys[pygame.K_ESCAPE]:
-            run = False
+            pause = True
         
         move_lasers(laser_velocity, player)
         
@@ -195,7 +222,7 @@ def main():
                 lives -= 1
                 enemies.remove(enemy)
                 
-        player.move_lasers(-laser_velocity2, enemies)
+        player.move_lasers(-laser_velocity_player, enemies)
         
         
         
